a10b31c15c044f50d480e2cf305a2f8b1ab80dda
Zane Zelenina <zane.zelenina@cgi.com>
2021-09-11 19:30:23 +0300

    Added tasks 4.-8.

--- ConsoleApp2-HW/Program.cs
+++ ConsoleApp2-HW/Program.cs
@@ -26,23 +26,87 @@ namespace ConsoleApp2_HW
 
             Console.WriteLine();
             Console.WriteLine($"Nākamgad Tev paliks {userAgeNextYear} gadi! Vai Tu esi pilngadīgs: {is18Age}");
-        
+
 
             Console.WriteLine("\n========== 3. uzdevums ==========\n");
 
             Console.Write("Lūdzu ievadiet pirmo skaitli:");
-            string userNumber1 = Console.ReadLine();
+            string userMaxNumber1 = Console.ReadLine();
 
             Console.Write("Lūdzu ievadiet otro skaitli:");
-            string userNumber2 = Console.ReadLine();
+            string userMaxNumber2 = Console.ReadLine();
 
-            int maxLargest = Math.Max(int.Parse(userNumber1), int.Parse(userNumber2));
+            int maxLargest = Math.Max(int.Parse(userMaxNumber1), int.Parse(userMaxNumber2));
 
             Console.WriteLine();
             Console.WriteLine($"Lielākais ievadītais skaitlis ir {maxLargest}.");
 
             Console.WriteLine("\n========== 4. uzdevums ==========\n");
-            // TODO: 
+
+            Console.Write("Lūdzu ievadiet pirmo skaitli:");
+            string userMinNumber1 = Console.ReadLine();
+
+            Console.Write("Lūdzu ievadiet otro skaitli:");
+            string userMinNumber2 = Console.ReadLine();
+
+            int min = Math.Min(int.Parse(userMinNumber1), int.Parse(userMinNumber2));
+
+            Console.WriteLine();
+            Console.WriteLine($"Mazākais ievadītais skaitlis ir {min}.");
+
+            Console.WriteLine("\n========== 5. uzdevums ==========\n");
+
+            Console.Write("Lūdzu ievadiet pirmo skaitli:");
+            string userRemainderNumber1 = Console.ReadLine();
+
+            Console.Write("Lūdzu ievadiet otro skaitli:");
+            string userRemainderNumber2 = Console.ReadLine();
+
+            int remainderResult = int.Parse(userRemainderNumber1) % int.Parse(userRemainderNumber2);
+
+            Console.WriteLine();
+            Console.WriteLine($"Skaitļu dalījuma atlikums ir {remainderResult}.");
+
+            Console.WriteLine("\n========== 6. uzdevums ==========\n");
+
+            Console.Write("Lūdzu ievadiet skaitli:");
+            string userOddEvenNumber1 = Console.ReadLine();
+
+            int oddEvenResult = int.Parse(userOddEvenNumber1) % 2;
+            bool isEven = oddEvenResult == 0;
+
+            Console.WriteLine();
+            Console.WriteLine($"Vai ievadītais skaitlis ir pāra: {isEven}.");
+
+            Console.WriteLine("\n========== 7. uzdevums ==========\n");
+
+            Console.Write("Lūdzu ievadiet taisnstūra platumu:");
+            string rectangleWidth = Console.ReadLine();
+
+            Console.Write("Lūdzu ievadiet taisnstūra augstumu:");
+            string rectangleHeight = Console.ReadLine();
+
+            decimal rectangleArea = Math.Round(decimal.Parse(rectangleWidth) * decimal.Parse(rectangleHeight), 2);
+
+            Console.WriteLine();
+            Console.WriteLine($"Taisnstūra laukums ir {rectangleArea}.");
+
+            Console.WriteLine("\n========== 8. uzdevums ==========\n");
+
+            Console.Write("Lūdzu ievadiet vienādsānu trijstūra pamata platumu:");
+            string triangleBase = Console.ReadLine();
+
+            Console.Write("Lūdzu ievadiet vienādsānu trijstūra augstumu:");
+            string triangleHeight = Console.ReadLine();
+
+            decimal triangleArea = Math.Round((decimal.Parse(triangleBase) * decimal.Parse(triangleHeight)) / 2, 2);
+
+            Console.WriteLine();
+            Console.WriteLine($"Vienādsānu trijstūra laukums ir {triangleArea}.");
+
+            //Console.WriteLine("\n========== 9. uzdevums ==========\n");
+
+            // See first task
         }
     }
 }
